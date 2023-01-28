5dfc60b59a139e421cb5a1487f6070847d6f049c
Eren Erisgen <ederisgen@gmail.com>
2023-01-28 16:13:15 -0600

    added html files

--- garden/thegarden/views.py
+++ garden/thegarden/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def index(request):
     return render(request, "thegarden/index.html")
 
+
+def login_view(request):
+    if request.method == "POST":
+        pass
+    else:
+        return render(request, "thegarden/login.html")
