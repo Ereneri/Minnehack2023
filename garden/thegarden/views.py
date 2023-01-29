667f5c52ec1cc3762ec679f772e4e5151dc6dd9e
Eren Erisgen <ederisgen@gmail.com>
2023-01-29 01:03:13 -0600

    updated register logic

--- garden/thegarden/views.py
+++ garden/thegarden/views.py
@@ -38,46 +38,49 @@ def login_view(request):
             return render(request, "thegarden/login.html")
 
 def register_view(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        email = request.POST["email"]
-        password = request.POST["password"]
-        state = request.POST["state"]
-
-        # Attempt to create new user
-        try:
-            user = Account.objects.create_user(email, username, state, password)
-            listofitems = [
-                "Volunteered or participated in community service",
-                "Shoveled or mowed for a neighbor",
-                "Baby or dog sat for a neighbor",
-                "Visited a nursing home",
-                "Send cards to soldiers/elderly",
-                "Volunteer at a church or social event",
-                "Donated non perishable food to a charity drive",
-                "Donated money to charity",
-                "Donated supplies to a local nonprofit",
-                "Planted a tree or plant",
-                "Cleaned up trash at a local park",
-                "Used public transportation or a carpool",
-                "Adopted a pet from an animal shelter",
-                "Attended a community gathering",
-                "Started a community social group",
-                "Met a new neighbor",
-                "Help someone register to vote"
-            ]
-            for item in listofitems:
-                task = Task(user=user, title=item)
-                task.save()
-            user.save()
-            login(request, user)
-        except IntegrityError:
-            return render(request, "thegarden/register.html", {
-                "message": "Invalid username and/or email."
-            })
-        return HttpResponseRedirect(reverse("index"))
+    if request.user.is_authenticated:
+        return HttpResponseRedirect("/")
     else:
-        return render(request, "thegarden/register.html")
+        if request.method == "POST":
+            username = request.POST["username"]
+            email = request.POST["email"]
+            password = request.POST["password"]
+            state = request.POST["state"]
+
+            # Attempt to create new user
+            try:
+                user = Account.objects.create_user(email, username, state, password)
+                listofitems = [
+                    "Volunteered or participated in community service",
+                    "Shoveled or mowed for a neighbor",
+                    "Baby or dog sat for a neighbor",
+                    "Visited a nursing home",
+                    "Send cards to soldiers/elderly",
+                    "Volunteer at a church or social event",
+                    "Donated non perishable food to a charity drive",
+                    "Donated money to charity",
+                    "Donated supplies to a local nonprofit",
+                    "Planted a tree or plant",
+                    "Cleaned up trash at a local park",
+                    "Used public transportation or a carpool",
+                    "Adopted a pet from an animal shelter",
+                    "Attended a community gathering",
+                    "Started a community social group",
+                    "Met a new neighbor",
+                    "Help someone register to vote"
+                ]
+                for item in listofitems:
+                    task = Task(user=user, title=item)
+                    task.save()
+                user.save()
+                login(request, user)
+            except IntegrityError:
+                return render(request, "thegarden/register.html", {
+                    "message": "Invalid username and/or email."
+                })
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "thegarden/register.html")
 
 def garden_view(request):
     return render(request, "thegarden/index.html")
